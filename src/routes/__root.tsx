99c57f2478098abfa59dbfddcc5a76553a53a1cf
Pasqual Koschmieder <git@derklaro.dev>
2025-01-04 14:00:40 +0100

    add tanstack query/router devtools, update dependencies

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,45 @@
 import { ErrorDisplay } from "@/components/ErrorDisplay.tsx";
 import type { QueryClient } from "@tanstack/react-query";
-import { createRootRouteWithContext } from "@tanstack/react-router";
+import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import { Suspense, lazy } from "react";
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()({
-  notFoundComponent: () => <ErrorDisplay errorMessage={"The requested page does not exist"} />,
+  component: Root,
+  notFoundComponent: () => <ErrorDisplay errorMessage={"The requested page does not exist"}/>,
 });
+
+/**
+ * Component for the TanStack router devtools, only displayed in dev.
+ */
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null
+  : lazy(async () => {
+    const res = await import("@tanstack/router-devtools");
+    return {
+      default: res.TanStackRouterDevtools,
+    };
+  });
+
+/**
+ * Component for the TanStack query devtools, only displayed in dev.
+ */
+const TanStackQueryDevtools = import.meta.env.PROD
+  ? () => null
+  : lazy(async () => {
+    const res = await import("@tanstack/react-query-devtools");
+    return {
+      default: res.ReactQueryDevtools,
+    };
+  });
+
+function Root() {
+  return (
+    <>
+      <Outlet/>
+      <Suspense>
+        <TanStackRouterDevtools position={"bottom-left"}/>
+        <TanStackQueryDevtools buttonPosition={"bottom-right"}/>
+      </Suspense>
+    </>
+  );
+}
