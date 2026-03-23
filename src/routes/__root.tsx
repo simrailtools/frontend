5267ce5b5374a7809c56c012a50fec8f498ea325
Pasqual Koschmieder <git@derklaro.dev>
2026-03-23 19:27:08 +0100

    fix

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { TanStackDevtools } from "@tanstack/react-devtools";
-import { FormDevtoolsPanel } from "@tanstack/react-form-devtools";
-import { PacerDevtoolsPanel } from "@tanstack/react-pacer-devtools";
+import { formDevtoolsPlugin } from "@tanstack/react-form-devtools";
+import { pacerDevtoolsPlugin } from "@tanstack/react-pacer-devtools";
 import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
@@ -22,8 +22,8 @@ function Root() {
           position: "bottom-left",
         }}
         plugins={[
-          { name: "TanStack Form", render: <FormDevtoolsPanel /> },
-          { name: "TanStack Pacer", render: <PacerDevtoolsPanel /> },
+          formDevtoolsPlugin(),
+          pacerDevtoolsPlugin(),
           { name: "TanStack Query", render: <ReactQueryDevtoolsPanel /> },
           { name: "TanStack Router", render: <TanStackRouterDevtoolsPanel /> },
         ]}
